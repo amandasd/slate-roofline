4acd15eeddb2c75c1d8b76080ba8e0a560ed8be9
Amanda Dufek <amanda.dufek@gmail.com>
2020-06-24 13:21:34 -0700

    Adding print_roofline.py file

---
+++ scripts/print_roofline.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import sys
+import os
+import argparse
+
+################################################################################
+# Usage:
+################################################################################
+# usage: print_roofline.py [-h] -p PROJECT_DIR 
+#
+#optional arguments:
+#  -h, --help            show this help message and exit
+#  -p PROJECT_DIR, --project-dir PROJECT_DIR 
+#                        project-dir name
+###
+#
+# Example:
+#
+#    python scripts/print_roofline.py -p $SCRATCH/project/aries-roofline/matrix-add/outputs/matrix_advisor-1-16-512x8192/
+################################################################################
+
+# Add advisor dir to sys.path
+sys.path.append(os.environ['ADVISOR_2020_DIR']+'/pythonapi/')
+
+import advisor
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--project-dir", required=True, help="project-dir name")
+args = parser.parse_args()
+
+variables = ['self_ai', 'self_gflops', 'self_gflop', 'self_time', 'self_dram_gb', 'self_dram_loaded_gb', 'self_dram_stored_gb', 'self_memory_gb', 'self_loaded_gb', 'self_stored_gb', 'self_l2_gb', 'self_l3_gb', 'loop_name']
+
+project = advisor.open_project(args.project_dir)
+data = project.load(advisor.SURVEY)
+rows = [{col: row[col] for col in row} for row in data.bottomup]
+df = pd.DataFrame(rows).replace('', np.nan)
+
+for var in variables:
+    if 'loop_name' not in var:
+        df[var] = df[var].astype(float)
+
+print(df[variables].dropna().sort_values(by=['self_dram_gb'],ascending=False).head(5))
+
