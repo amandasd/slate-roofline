6182d63caf3e8df29d4ad8babc0ffe8c3f264037
Amanda S. Dufek <amanda.dufek@gmail.com>
2021-02-12 21:54:25 -0300

    Updating matrix_add_cpu.c

--- source/matrix-add-gpu/matrix_add_cpu.c
+++ source/matrix-add-gpu/matrix_add_cpu.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "timemory-interface/interface.h"
+
 int 
 matrix_vt_add(int num, float *m, float *n, float *r)
 {
@@ -36,7 +38,14 @@ matrix_vt_print(int nlin, int ncol, float *m)
 
 int main(int argc, char **argv) 
 {
+    set_papi_events(event_count, events);
 
+    int  size, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    push_region("main");
     int  nlin, ncol;
     if (argc < 3)
     {       
@@ -48,11 +57,6 @@ int main(int argc, char **argv)
          nlin = atoi(argv[1]);
          ncol = atoi(argv[2]);
     }
-    
-    int  size, rank;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
     float *matrix_C = (float*)malloc(nlin*ncol*sizeof(float));
     if(matrix_C == NULL) {
@@ -96,7 +100,8 @@ int main(int argc, char **argv)
     double t_start = 0.0, t_end = 0.0, t = 0.0;
     t_start = MPI_Wtime();
 #endif
-    
+    push_region("profiling"); 
+
     for(int i = 0; i < niter; i++) {
         if(matrix_vt_add((nlin/size)*ncol,vec_A,vec_B,vec_C)) {
             fprintf(stderr,"Rank %d: Error in addition operation.\n",rank);
@@ -120,11 +125,14 @@ int main(int argc, char **argv)
     
     // Collective MPI routines
     MPI_Allgather(vec_C,(nlin/size),rowtype,matrix_C,(nlin/size),rowtype,MPI_COMM_WORLD);
-    
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    pop_region("profiling"); 
+
 #ifdef PROFILING
     MPI_Barrier(MPI_COMM_WORLD);
     t_end = MPI_Wtime();
-    t = t_end - t_start;
+    t     = t_end - t_start;
 #endif
     
 #ifdef PROFILING
@@ -146,7 +154,9 @@ int main(int argc, char **argv)
     free(vec_A);
     free(vec_B);
     free(vec_C);
-    
+
+    pop_region("main");   
+    finalize();
     MPI_Finalize();
      
     return 0;
